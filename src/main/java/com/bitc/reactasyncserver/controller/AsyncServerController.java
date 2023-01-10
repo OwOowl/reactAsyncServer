3388b74279be9453a8d51645874e4cc31c811675
yang <ymh0052@naver.com>
2023-01-10 16:06:29 +0900

    CORS 오류, 접근 설정

--- src/main/java/com/bitc/reactasyncserver/controller/AsyncServerController.java
+++ src/main/java/com/bitc/reactasyncserver/controller/AsyncServerController.java
@@ -1,12 +1,14 @@
 package com.bitc.reactasyncserver.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/async")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4000"})
 public class AsyncServerController {
     @RequestMapping("/")
     public String index() throws Exception {
@@ -14,10 +16,61 @@ public class AsyncServerController {
     }
 
 // 해당 주소 접근 허용
-    @CrossOrigin(origins = "http://localhost:3000")
-    @ResponseBody
-    @RequestMapping(value = "/async/data1", method = RequestMethod.POST)
-    public String asyncData1() throws Exception {
-        return "서버와 통신 성공";
+//    CORS 오류 : Cross Origin Resource Sharing의 줄임말. 동일한 주소내에서만 리소스를 공유할 수 있음
+//    스프링에서는 CORS 허용을 위해서 @CrossOrigin 어노테이션을 사용하여 처리함
+//    @CrossOrigin 어노테이션을 사용하면 지정한 도메인에 대해서 접근을 허용함
+//    @CrossOrigin 어노테이션은 메소드, 클래스, configurer에 설정할 수 있음
+//    메소드에 사용 시 지정한 메소드만 접근을 허용함
+//    클래스에 사용 시 지정한 컨트롤러에 대해서만 접근을 허용함
+//    configurer에 사용 시 모든 곳에 접근을 허용함
+//    옵션으로 origins에 접근할 서버의 주소를 입력함
+
+
+    @RequestMapping(value = "/data1", method = RequestMethod.POST)
+    public Object asyncData1() throws Exception {
+        return "success";
+    }
+
+
+    @RequestMapping(value = "/sendDataGet", method = RequestMethod.GET)
+    public Object sendDataGet(@RequestParam("id") String id, @RequestParam("pw") String pw) throws Exception {
+        Map<String, String> user = new HashMap<>();
+        user.put("id", id);
+        user.put("pw", pw);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("result", "success");
+        result.put("data", user);
+
+        return result;
+    }
+
+    @RequestMapping(value = "/sendDataPost", method = RequestMethod.POST)
+    public Object sendDataPost(@RequestParam("id") String id, @RequestParam("pw") String pw) throws Exception {
+        Map<String, String> user = new HashMap<>();
+        user.put("id", id);
+        user.put("pw", pw);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("result", "success");
+        result.put("data", user);
+
+        return result;
+    }
+
+    @RequestMapping(value = "/sendDataPut", method = RequestMethod.PUT)
+    public Object sendDataPut(@RequestParam("idx") int idx) throws Exception {
+        Map<String, Object> result = new HashMap<>();
+        result.put("result", "success");
+        result.put("idx", idx);
+        return result;
+    }
+
+    @RequestMapping(value = "/sendDataDelete", method = RequestMethod.DELETE)
+    public Object sendDataDelete(@RequestParam("idx") int idx) throws Exception {
+        Map<String, Object> result = new HashMap<>();
+        result.put("result", "success");
+        result.put("idx", idx);
+        return result;
     }
 }
